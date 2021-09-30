e92c8d664a1a86050a302c0af7c7c0152e909cb0
Opilane TTHK <opilane@tthk.ee>
2021-09-30 11:27:34 +0300

    Week5-MoreLoops

---
+++ ForLoops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis
+            //"I will not skateboard in the hall"
+            //10 korda
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i + 1}I will not skateboard in the hall");
+            }
+        }
+    }
+}
---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+            //katsete arv on piiramatu
+            //programm genereerib juhuslikku numbrit ühte korda.
+
+            Random rnd = new Random();
+            int cpuRandom = rnd.Next(1, 11);
+            bool loopActive = true;
+
+            
+
+            while (loopActive)
+            {
+                Console.WriteLine("Vali number 1-10");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                if(cpuRandom == userNumber)
+                {
+                    Console.WriteLine("Palju õnne! Oled võitnud!");
+                    //break;
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Sa kaotasid, proovi uuesti");
+                }
+            }
+
+            Console.WriteLine("Kena päeva");
+
+        }
+    }
+}
